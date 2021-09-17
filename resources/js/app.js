36d3fb1e3976a0d1330b17842fed00e73b4f6b47
Alisher <88429349+ibnAbdulatif@users.noreply.github.com>
2021-09-17 17:33:04 +0500

    add Dashboard & Profile list

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,27 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue').default;
+window.Vue = require("vue").default;
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+let routes = [
+    {
+        path: "/dashboard",
+        component: require("./components/Dashboard.vue").default
+    },
+    {
+        path: "/profile",
+        component: require("./components/Profile.vue").default
+    }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes // short for `routes: routes`
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +37,10 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +49,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    router
 });
