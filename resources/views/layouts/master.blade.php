6ac9daa74bff0cb71f2c5da058f9df29fa93a658
Alisher <88429349+ibnAbdulatif@users.noreply.github.com>
2021-09-17 19:17:48 +0500

    realize logout button

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,7 +74,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Management
@@ -83,7 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link ">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
@@ -105,12 +105,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
               <i class="nav-icon fas fa-power-off"></i>
               <p>
-                Log Out
+                {{ __('Logout') }}
               </p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
           </li>
         </ul>
       </nav>
